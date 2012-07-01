fccbe08f9a4ecfc375f296384a946415811e33a0
Holger Just <hjust@meine-er.de>
2012-07-01 13:55:49 +0200

    We do in fact need git for installing ChiliProject

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,8 @@ self.class.send(:include, ChiliProject::Helpers)
 include_recipe "chiliproject::database"
 
 # Then deploy the ChiliProject instances
+include_recipe "git"
+
 instances = Chef::DataBag.load("chiliproject").values
 instances.each do |inst|
   chiliproject inst['id'] do
